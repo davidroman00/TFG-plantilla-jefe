efb9ab0a20f6ad4a56ab130fb64e0f33ab8b8b0f
davidroman00 <davidro3421@gmail.com>
2024-03-16 13:34:02 +0100

    añado movimiento basico

--- Assets/Character/Scripts/PlayerController.cs
+++ Assets/Character/Scripts/PlayerController.cs
@@ -1,18 +1,47 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private int _speed = 10;
+    CharacterController _characterController;
+    PlayerControlls _playerControlls;
+    Vector2 _movementInput;
+    Vector3 _movementApplied;
+    bool _isMoving;
+    bool _isAttackPressed;
+    bool _isBackdashPressed;
 
-    // Update is called once per frame
+
+    void Awake()
+    {
+        _characterController = GetComponent<CharacterController>();
+        _playerControlls = new PlayerControlls();
+        _playerControlls.Default.Movement.started += onMovementInput;
+        _playerControlls.Default.Movement.canceled += onMovementInput;
+    }
     void Update()
     {
-        
+        _characterController.Move(_movementApplied * Time.deltaTime * _speed);
+        Debug.Log(_isMoving);
+    }
+
+    void onMovementInput(InputAction.CallbackContext context){
+        _movementInput = context.ReadValue<Vector2>();
+        _movementApplied.x = _movementInput.x;
+        _movementApplied.z = _movementInput.y;
+        _isMoving = _movementApplied.x != 0 || _movementApplied.z != 0;
+    }
+    void OnEnable()
+    {
+        _playerControlls.Default.Enable();
+    }
+    void OnDisable()
+    {
+        _playerControlls.Default.Disable();
     }
 }

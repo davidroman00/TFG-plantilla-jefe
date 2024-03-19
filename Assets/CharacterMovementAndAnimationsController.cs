4343287ebc429c06ab4b0e1a72dd16accf77440c
davidroman00 <davidro3421@gmail.com>
2024-03-19 19:11:03 +0100

    implementado todo el sistema de movimientos y animaciones correctamente + eliminación de la animación y estado de muerte

--- Assets/CharacterMovementAndAnimationsController.cs
+++ Assets/CharacterMovementAndAnimationsController.cs
@@ -8,29 +8,52 @@ public class CharacterMovementAndAnimationsController : MonoBehaviour
     float Speed = 10f;
     float _turnSmoothTime = .1f;
     float _turnSmoothVelocity;
+    float _horizontalInput;
+    float _verticalInput;
+    Vector3 _initialDirection;
+    Vector3 _moveDirection;
     [SerializeField]
     Transform Camera;
     CharacterController _characterController;
+    Animator _animator;
 
     void Awake()
     {
         _characterController = GetComponent<CharacterController>();
+        _animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3 (horizontal, 0f, vertical).normalized;
+        HandleInput();  
 
-        if (direction.magnitude > .1f){
+        if (_initialDirection.magnitude > .1f){
+            HandlePlayerMovementAndRotation();
+            _animator.SetBool("isWalking", true);            
+        } else {
+            _animator.SetBool("isWalking", false);
+        } 
+    }
 
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + Camera.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, _turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+    void HandleInput(){
+        _horizontalInput = Input.GetAxisRaw("Horizontal");
+        _verticalInput = Input.GetAxisRaw("Vertical");
+        _initialDirection = new Vector3 (_horizontalInput, 0f, _verticalInput).normalized;
 
-            Vector3 moveDirection = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
-            _characterController.Move(moveDirection.normalized * Speed * Time.deltaTime);
-        }
+        if(Input.GetKeyDown("space")){
+            _animator.SetTrigger("Backdashed");
+        } 
+        if(Input.GetMouseButtonDown(0)){
+            _animator.SetTrigger("Attacked");
+        } 
+
+    }
+    void HandlePlayerMovementAndRotation(){
+        float targetAngle = Mathf.Atan2(_initialDirection.x, _initialDirection.z) * Mathf.Rad2Deg + Camera.eulerAngles.y;
+        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, _turnSmoothTime);
+        transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+        _moveDirection = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+        _characterController.Move(_moveDirection.normalized * Speed * Time.deltaTime);
     }
 }

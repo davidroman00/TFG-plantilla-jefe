d45a81ea4df4d24ba61810512705ae0dd3cd9619
davidroman00 <davidro3421@gmail.com>
2024-04-09 17:24:35 +0200

    fix en el script de la rotacion del jefe, que daba problemas

--- Assets/Boss/Scripts/BossLookAt.cs
+++ Assets/Boss/Scripts/BossLookAt.cs
@@ -4,24 +4,18 @@ using UnityEngine;
 
 public class BossLookAt : StateMachineBehaviour
 {
-    Rigidbody _bossRigidbody;
     Transform _playerTransform;
     Vector3 _playerPosition;
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         _playerTransform = animator.GetComponent<BossReferences>().PlayerTransform;
-        _bossRigidbody = animator.GetComponent<Rigidbody>();
     }
 
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        HandleBossRotation();
-    }
-    void HandleBossRotation()
-    {
         _playerPosition.x = _playerTransform.position.x;
         _playerPosition.z = _playerTransform.position.z;
-        Quaternion appliedRotation = Quaternion.LookRotation(_playerPosition, Vector3.up);
-        _bossRigidbody.MoveRotation(appliedRotation.normalized);
+        animator.transform.LookAt(_playerPosition);
     }
+
 }

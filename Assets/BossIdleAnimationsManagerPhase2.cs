9fd6b0949784b819b060be9669ccdc12a088e8ab
davidroman00 <davidro3421@gmail.com>
2024-04-19 14:57:55 +0200

    setup de animaciones para fase 2 del jefe

---
+++ Assets/BossIdleAnimationsManagerPhase2.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossIdleAnimationsManagerPhase2 : StateMachineBehaviour
+{
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+       
+    }
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+       
+    }
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+       
+    }
+
+}

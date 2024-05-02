c75c18a7e17ff7e322f9d82f60e3eb7d6baf52ab
davidroman00 <davidro3421@gmail.com>
2024-05-02 16:48:24 +0200

    corrección final al backdash

--- Assets/Boss/Scripts/Animator States Scripts/Global/BossBackdashStateManager.cs
+++ Assets/Boss/Scripts/Animator States Scripts/Global/BossBackdashStateManager.cs
@@ -13,22 +13,22 @@ public class BossBackdashStateManager : StateMachineBehaviour
         _bossReferences = animator.GetComponent<BossReferences>();
         animator.GetComponent<BossCooldownManager>().LastBackdash = Time.time;
 
-        _playerPosition.x = _bossReferences.PlayerTransform.position.x;
+        //We only need the Z axis for this movement.
         _playerPosition.z = _bossReferences.PlayerTransform.position.z;
-        _playerPosition.x = -_playerPosition.x;
-        _playerPosition.z = -_playerPosition.z;        
-        _bossPosition.x = animator.transform.position.x;
+        _playerPosition.z = -_playerPosition.z;
         _bossPosition.z = animator.transform.position.z;
 
         //This line here is calculating the direction vector to be applied.
-        _moveDirection = _playerPosition - _bossPosition;        
+        _moveDirection = _playerPosition - _bossPosition;
+
     }
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {     
+    {
         if (_bossStats.IsActualBackdashActive && !_bossStats.IsOutsideArena)
+        //Since the boss is moved through transform.position, it is important to check if the boss is within the arena during the movement, otherwise it could clip thorugh walls.
         {
             animator.transform.Translate(_bossStats.DashMovementSpeed * Time.deltaTime * _moveDirection.normalized);
-        }        
+        }
     }
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {

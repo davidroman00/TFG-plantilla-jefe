2a08665eca6732fc483e9f98a70f96cf4be368f8
davidroman00 <davidro3421@gmail.com>
2024-05-02 13:26:38 +0200

    solucion temporal al backdash (atraviesa muros)

--- Assets/Boss/Scripts/Animator States Scripts/Global/BossBackdashStateManager.cs
+++ Assets/Boss/Scripts/Animator States Scripts/Global/BossBackdashStateManager.cs
@@ -5,25 +5,36 @@ public class BossBackdashStateManager : StateMachineBehaviour
     BossStats _bossStats;
     BossReferences _bossReferences;
     Vector3 _playerPosition;
+    Vector3 _bossPosition;
+    Vector3 _moveDirection;
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         _bossStats = animator.GetComponent<BossStats>();
         _bossReferences = animator.GetComponent<BossReferences>();
         animator.GetComponent<BossCooldownManager>().LastBackdash = Time.time;
-    }
-    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
+
         _playerPosition.x = _bossReferences.PlayerTransform.position.x;
         _playerPosition.z = _bossReferences.PlayerTransform.position.z;
         _playerPosition.x = -_playerPosition.x;
         _playerPosition.z = -_playerPosition.z;
+        
+        _bossPosition.x = animator.transform.position.x;
+        _bossPosition.z = animator.transform.position.z;
+
+        _moveDirection = _playerPosition - _bossPosition;        
+    }
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {     
         if (_bossStats.IsActualBackdashActive)
         {
-            animator.transform.position = Vector3.MoveTowards(animator.transform.position, _playerPosition, _bossStats.DashMovementSpeed * Time.deltaTime);
-        }
+            animator.transform.Translate(_bossStats.DashMovementSpeed * Time.deltaTime * _moveDirection.normalized);
+        }        
     }
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         animator.ResetTrigger("backdash");
+        _playerPosition = Vector3.zero;
+        _bossPosition = Vector3.zero;
+        _moveDirection = Vector3.zero;
     }
 }
